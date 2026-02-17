a14a57ed3de11905056bf41a349ded201ec82c56
Rafael Carlos Rosa <rafaelcarlosr@gmail.com>
2026-02-17 19:30:02 -0300

    test changes

--- src/main/java/com/taller/test/TestQuestion.java
+++ src/main/java/com/taller/test/TestQuestion.java
@@ -1,6 +1,20 @@
 package com.taller.test;
+import java.math.BigDecimal;
 
 public class TestQuestion {
+    static void main() {
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
 
+        paymentProcessor.addPayment(new Payment("1", BigDecimal.valueOf(100), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("2", BigDecimal.valueOf(200), "BRL", Payment.Status.PENDING));
+        paymentProcessor.addPayment(new Payment("3", BigDecimal.valueOf(300), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("4", BigDecimal.valueOf(400), "USD", Payment.Status.PENDING));
+        paymentProcessor.addPayment(new Payment("5", BigDecimal.valueOf(500), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("6", BigDecimal.valueOf(600), "USD", Payment.Status.PENDING));
+        paymentProcessor.addPayment(new Payment("7", BigDecimal.valueOf(700), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("8", BigDecimal.valueOf(800), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("9", BigDecimal.valueOf(900), "USD", Payment.Status.SUCCESS));
+        paymentProcessor.addPayment(new Payment("10", BigDecimal.valueOf(1000), "USD", Payment.Status.SUCCESS));
 
+    }
 }
